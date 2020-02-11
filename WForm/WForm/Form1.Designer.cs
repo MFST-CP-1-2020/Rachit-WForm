bada930fcf198ec0d8427d755ecbaa2fc4240311
RJ218 <rachitjain218@gmail.com>
2020-02-11 16:36:40 +0530

    implemented the code by dividing the code into various functions and added button ui to the output grid.

--- WForm/WForm/Form1.Designer.cs
+++ WForm/WForm/Form1.Designer.cs
@@ -39,19 +39,19 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox9 = new System.Windows.Forms.TextBox();
             this.c = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.label11 = new System.Windows.Forms.Label();
-            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.c)).BeginInit();
             this.SuspendLayout();
             // 
@@ -128,21 +128,6 @@
             this.textBox6.Size = new System.Drawing.Size(100, 22);
             this.textBox6.TabIndex = 8;
             // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(156, 265);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 22);
-            this.textBox7.TabIndex = 9;
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(381, 280);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 22);
-            this.textBox8.TabIndex = 10;
-            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
@@ -164,7 +149,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(315, 212);
+            this.label6.Location = new System.Drawing.Point(299, 212);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(60, 17);
             this.label6.TabIndex = 13;
@@ -173,7 +158,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(66, 268);
+            this.label7.Location = new System.Drawing.Point(66, 283);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(56, 17);
             this.label7.TabIndex = 14;
@@ -182,7 +167,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(333, 283);
+            this.label8.Location = new System.Drawing.Point(299, 283);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(41, 17);
             this.label8.TabIndex = 15;
@@ -192,18 +177,12 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(315, 345);
+            this.label9.Location = new System.Drawing.Point(299, 348);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(57, 17);
             this.label9.TabIndex = 16;
             this.label9.Text = "Country";
-            // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(381, 340);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 22);
-            this.textBox9.TabIndex = 17;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // c
             // 
@@ -235,33 +214,63 @@
             this.label11.TabIndex = 21;
             this.label11.Text = "Employee Registration Form";
             // 
-            // cb1
+            // comboBox1
             // 
-            this.cb1.FormattingEnabled = true;
-            this.cb1.Location = new System.Drawing.Point(683, 385);
-            this.cb1.Name = "cb1";
-            this.cb1.Size = new System.Drawing.Size(121, 24);
-            this.cb1.TabIndex = 22;
-            this.cb1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(360, 283);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 23;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(362, 345);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 24;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(156, 266);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 21);
+            this.radioButton1.TabIndex = 25;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(156, 293);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 21);
+            this.radioButton2.TabIndex = 26;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1523, 526);
-            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.c);
-            this.Controls.Add(this.textBox9);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -291,19 +300,19 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.DataGridView c;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- WForm/WForm/Form1.cs
+++ WForm/WForm/Form1.cs
@@ -13,11 +13,141 @@ namespace WForm
     public partial class Form1 : Form
     {
        // Form1 frm;
-        DataTable table = new DataTable();
+        DataTable user = new DataTable();
         DataTable dt = new DataTable();
+        DataTable state = new DataTable();
+        DataTable country = new DataTable();
         //DataColumn Fname = new DataColumn("first_name");
         //DataColumn Lname = new DataColumn("Last_name");
         //DataColumn Fthername
+
+        public void bindcontent()
+        {
+            bindstate();
+            bindcountry();
+            makeuser();
+            
+        }
+        public void addrow(int i,string s)
+        {
+            DataRow r = state.NewRow();
+            r["id"] = i;
+            r["Name"] = s;
+            state.Rows.Add(r);
+        }
+        public void bindstate()
+        {
+            state.Columns.Add("id", typeof(int));
+            state.Columns.Add("Name", typeof(string));
+
+            /*DataRow r = state.NewRow();
+            r["id"] = 1;
+            r["Name"] = "UttarPradesh";
+            state.Rows.Add(r);
+            DataRow r2 = state.NewRow();
+            r2["id"] = 2;
+            r2["Name"] = "Maharastra";
+            state.Rows.Add(r2);
+            DataRow r3 = state.NewRow();
+            r3["id"] = 3;
+            r3["Name"] = "Jammu";
+            state.Rows.Add(r3);
+
+            DataRow r4 = state.NewRow();
+            r4["id"] = 4;
+            r4["Name"] = "Kashmir";
+
+            state.Rows.Add(r4);
+            //state.Rows.Add(r);
+            */
+            addrow(0, "choose");
+            addrow(1, "UttarPradesh");
+            addrow(2, "Maharastra");
+            addrow(3, "Jammu");
+            addrow(4, "Kashmir");
+            comboBox1.DisplayMember = "name";
+            comboBox1.ValueMember = "id";
+            comboBox1.DataSource = state;
+        }
+        public void makeuser()
+        {
+
+            user.Columns.Add("First Name");
+            user.Columns.Add("Last Name");
+            user.Columns.Add("Father's Name");
+            user.Columns.Add("Mother's Name");
+            user.Columns.Add("Phone Number");
+            user.Columns.Add("Address");
+            user.Columns.Add("State");
+            user.Columns.Add("Country");
+            user.Columns.Add("Gender");
+            user.Columns.Add("Load");
+            //DataGridViewButtonColumn bttn_load = new DataGridViewButtonColumn();
+            //c.Columns.Add(bttn_load);
+
+        }
+        public void add_to_user()
+        { 
+            DataRow row = user.NewRow();
+            row[0] = textBox1.Text;
+            row[1] = textBox2.Text;
+            row[2] = textBox3.Text;
+            row[3] = textBox4.Text;
+            row[4] = textBox5.Text;
+            row[5] = textBox6.Text;
+            row["State"] = comboBox1.Text;
+            row["Country"] = comboBox2.Text;
+            string gender;
+            bool ischecked = radioButton1.Checked;
+            if (ischecked)
+            {
+                gender = radioButton1.Text;
+            }
+            else
+                gender = radioButton2.Text;
+            row[8] = gender;
+            user.Rows.Add(row);
+            c.DataSource = user;
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+
+            string value = country.Rows[0]["name"].ToString();
+
+            comboBox1.Text = value;
+            comboBox2.Text = value;
+            DataGridViewButtonColumn bttn_load = new DataGridViewButtonColumn();
+            bttn_load.HeaderText = "Load data";
+            bttn_load.Name = "bttn_load";
+            bttn_load.UseColumnTextForButtonValue = true;
+          //  if (c.Columns.Contains(bttn_load.Name = "bttn_load"))
+            //{
+            //
+            //}
+            //else
+                c.Columns.Add(bttn_load);
+            
+        }
+        public void bindcountry()
+        {
+
+            country.Columns.Add("id", typeof(int));
+            country.Columns.Add("name", typeof(string));
+            country.Rows.Add(0, "choose");
+            country.Rows.Add(1, "India");
+            country.Rows.Add(2, "America");
+            country.Rows.Add(3, "Australia");
+            country.Rows.Add(4, "England");
+            comboBox2.DisplayMember = "name";
+            comboBox2.ValueMember = "id";
+            comboBox2.DataSource = country;
+        }
+
+
         public Form1()
         {
 
@@ -42,26 +172,18 @@ namespace WForm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            dt.Columns.Add("Id");
-            dt.Columns.Add("Name");
+            //dt.Columns.Add("Id");
+            //dt.Columns.Add("Name");
 
-            DataRow r = dt.NewRow();
-            r["Id"] = 1;
-            r["Name"] = "one";
-
-            table.Columns.Add("First Name");
-            table.Columns.Add("Last Name");
-            table.Columns.Add("Father's Name");
-            table.Columns.Add("Mother's Name");
-            table.Columns.Add("Phone Number");
-            table.Columns.Add("Address");
-            table.Columns.Add("State");
-            table.Columns.Add("Country");
-            table.Columns.Add("Gender");
-            dt.Rows.Add(r);
-            cb1.DataSource = dt;
-            cb1.DisplayMember = "Name";
-            cb1.ValueMember = "Id";
+            //DataRow r = dt.NewRow();
+            //r["Id"] = 1;
+            //r["Name"] = "one";
+           bindcontent();
+            
+            //dt.Rows.Add(r);
+          //  cb1.DataSource = dt;
+            //cb1.DisplayMember = "Name";
+            //cb1.ValueMember = "Id";
 
         }
 
@@ -73,20 +195,30 @@ namespace WForm
         private void button1_Click(object sender, EventArgs e)
         {
             //table.c.Rows.Add(textBox1.Text);
-            DataRow row = table.NewRow();
-                        row[0] = textBox1.Text;
-            row[1] = textBox2.Text;
-            row[2] = textBox3.Text;
-            row[3] = textBox4.Text;
-            row[4] = textBox5.Text;
-            row[5] = textBox6.Text;
-            row[6] = textBox8.Text;
-            row[7] = textBox9.Text;
-            row[8] = textBox7.Text;
-            table.Rows.Add(row);
-            c.DataSource = table;
-            
-           
+            /* DataRow row = user.NewRow();
+             row[0] = textBox1.Text;
+             row[1] = textBox2.Text;
+             row[2] = textBox3.Text;
+             row[3] = textBox4.Text;
+             row[4] = textBox5.Text;
+             row[5] = textBox6.Text;
+             row["State"] = comboBox1.Text;
+             row["Country"] = comboBox2.Text;
+             string gender;
+             bool ischecked = radioButton1.Checked;
+             if (ischecked)
+             {
+                 gender = radioButton1.Text;
+             }
+             else
+                 gender = radioButton2.Text;
+             row[8] = gender;
+             user.Rows.Add(row);
+             c.DataSource = user;
+             */
+            //bindcontent();
+            add_to_user();
+
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -101,6 +233,16 @@ namespace WForm
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
         {
 
         }
